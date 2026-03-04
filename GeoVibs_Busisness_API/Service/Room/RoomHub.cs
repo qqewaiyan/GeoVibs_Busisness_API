a509a1909bfeb7cca89019e681314eaa80093748
qqewaiyan <waiyanphyomg2002@gmail.com>
2026-03-04 23:08:32 +0630

    AddingDocker

---
+++ GeoVibs_Busisness_API/Service/Room/RoomHub.cs
@@ -0,0 +1,13 @@
+﻿namespace GeoVibs_Busisness_API.Service.Room
+{
+    using Microsoft.AspNetCore.SignalR;
+
+    public class RoomHub : Hub
+    {
+        // Send updated room status to all clients
+        //public async Task SendRoomUpdate(RoomStatusDto room)
+        //{
+        //    await Clients.All.SendAsync("ReceiveRoomUpdate", room);
+        //}
+    }
+}
